6fe73042887ae834505d27109806b8745b138116
Jeremy <anon@anon.com>
2012-11-07 20:32:18 -0500

    Make DFA table have a char->column mapping

--- DFA.java
+++ DFA.java
@@ -3,15 +3,30 @@ import java.util.*;
 public class DFA extends FiniteAutomata {
 
 	private int[][] dfa;
+	private Map<Character, Integer> transitions;
+	private static char EPSILON = (char) 169;
 	
-	public DFA(int numStates, int numTransitions) {
+	public DFA(int numStates, Set<Character> transitions) {
 		this.numStates = numStates;
-		this.numTransitions = numTransitions;
+		this.numTransitions = transitions.size() - 1;
+		int i = 0;
+		Character[] trans = (Character[]) transitions.toArray();
+		Arrays.sort(trans);
+		for (Character c : trans) {
+			if (c != EPSILON) {
+				this.transitions.put(c, i);
+				i++;
+			}
+		}
 		dfa = new int[numStates][numTransitions];
 	}
 	
-	public void addTransition(int start, int end, int transition) {
-		dfa[start][transition] = end;
+	public void addTransition(int start, int end, char transition) {
+		dfa[start][transitions.get(transition)] = end;
+	}
+	
+	public int getNextState(int state, char transition) {
+		return dfa[state][transitions.get(transition)];
 	}
 	
 	public String toString() {
