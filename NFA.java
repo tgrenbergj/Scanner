11f8e74bd40d0c5a56a15e183bc322ba2a2096ba
Jeremy <anon@anon.com>
2012-11-12 15:01:39 -0500

    Removed all static methods, added some getters

--- NFA.java
+++ NFA.java
@@ -44,6 +44,10 @@ public class NFA extends FiniteAutomata {
 		addTransition(start, end, EPSILON);
 	}
 	
+	public List<Map<Character, Set<Integer>>> getNFA() {
+		return nfa;
+	}
+	
 	public Set<Character> getTransitions() {
 		return transitions;
 	}
@@ -63,7 +67,9 @@ public class NFA extends FiniteAutomata {
 	public Set<Integer> getNextStates(Set<Integer> states, char transition) {
 		Set<Integer> nextStates = new HashSet<Integer>();
 		for (Integer state : states) {
-			nextStates.addAll(nfa.get(state).get(transition));
+			if (nfa.get(state).containsKey(transition)) {
+				nextStates.addAll(nfa.get(state).get(transition));
+			}
 		}
 		return nextStates;
 	}
@@ -99,168 +105,6 @@ public class NFA extends FiniteAutomata {
 		}
 		return closure;
 	}
-	
-	public static NFA union(NFA nfa1, NFA nfa2) {
-		NFA newNFA = new NFA();
-		newNFA.addState();
-		newNFA.setStartState(0);
-		
-		int current = 1;
-		int offset = 1;
-		
-		for (Map<Character, Set<Integer>> map : nfa1.nfa) {
-			newNFA.addState();
-			for(Character c : map.keySet()) {
-				Set<Integer> next = map.get(c);
-				for (Integer end : next) {
-					newNFA.addTransition(current, end + offset, c);
-				}
-			}
-			current++;
-		}
-		newNFA.addEpsilonTransition(newNFA.getStartState(), nfa1.getStartState() + offset);
-		for (Integer state : nfa1.finalStates) {
-			newNFA.addFinalState(state + offset);
-		}
-		
-		offset = current;
-		
-		for (Map<Character, Set<Integer>> map : nfa2.nfa) {
-			newNFA.addState();
-			for(Character c : map.keySet()) {
-				Set<Integer> next = map.get(c);
-				for (Integer end : next) {
-					newNFA.addTransition(current, end + offset, c);
-				}
-			}
-			current++;
-		}
-		
-		newNFA.addEpsilonTransition(newNFA.getStartState(), nfa2.getStartState() + offset);
-		for (Integer state : nfa2.finalStates) {
-			newNFA.addFinalState(state + offset);
-		}
-		
-		return newNFA;
-	}
-	
-	public static NFA concat(NFA nfa1, NFA nfa2) {
-		NFA newNFA = new NFA();
-		
-		newNFA.setStartState(nfa1.getStartState());
-		
-		int current = 0;
-		int offset = 0;
-		
-		for (Map<Character, Set<Integer>> map : nfa1.nfa) {
-			newNFA.addState();
-			for(Character c : map.keySet()) {
-				Set<Integer> next = map.get(c);
-				for (Integer end : next) {
-					newNFA.addTransition(current, end + offset, c);
-				}
-			}
-			current++;
-		}
-		
-		offset = current;
-		
-		for (Map<Character, Set<Integer>> map : nfa2.nfa) {
-			newNFA.addState();
-			for(Character c : map.keySet()) {
-				Set<Integer> next = map.get(c);
-				for (Integer end : next) {
-					newNFA.addTransition(current, end + offset, c);
-				}
-			}
-			current++;
-		}
-		
-		for (Integer state : nfa1.getFinalStates()) {
-			newNFA.addEpsilonTransition(state, nfa2.getStartState() + offset);
-		}
-		
-		for (Integer state : nfa2.getFinalStates()) {
-			newNFA.addFinalState(state + offset);
-		}				
-		
-		return newNFA;
-	}
-	
-	public static NFA star(NFA nfa1) {
-		NFA newNFA = new NFA();
-		newNFA.addState();
-		newNFA.setStartState(0);
-		newNFA.addFinalState(0);
-		int offset = 1;
-		int current = 1;
-		
-		for (Map<Character, Set<Integer>> map : nfa1.nfa) {
-			newNFA.addState();
-			for(Character c : map.keySet()) {
-				Set<Integer> next = map.get(c);
-				for (Integer end : next) {
-					newNFA.addTransition(current, end + offset, c);
-				}
-			}
-			current++;
-		}
-		
-		for (Integer end : nfa1.getFinalStates()) {
-			newNFA.addFinalState(end + offset);
-			newNFA.addEpsilonTransition(end + offset, nfa1.getStartState() + offset);
-		}
-		
-		newNFA.addEpsilonTransition(newNFA.getStartState(), nfa1.getStartState() + offset);
-		
-		return newNFA;
-	}
-	
-	public static NFA plus(NFA nfa1) {
-		return NFA.concat(nfa1, NFA.star(nfa1));
-	}
-	
-	//NOTE: ONLY WORKS ON CHARACTER CLASSES
-	public static NFA compress(NFA nfa1) {
-		NFA newNFA = new NFA();
-		newNFA.addState();
-		newNFA.setStartState(0);
-		int current = 1;
-		for (Character c: nfa1.transitions) {
-			if ( c != EPSILON) {
-				newNFA.addState();
-				newNFA.addTransition(newNFA.getStartState(), current, c);
-				newNFA.addFinalState(current);
-				current++;
-			}
-		}
-		return newNFA;
-	}
-	
-	//nfa1 is the old set, and nfa2 is what I want to subtract from nfa1
-	public static NFA minus(NFA nfa1, NFA nfa2) {
-		Set<Character> nfa1set = nfa1.transitions;
-		Set<Character> nfa2set = nfa2.transitions;
-		Set<Character> newset = new HashSet<Character>();
-		for (Character c: nfa1set) {
-			if (!nfa2set.contains(c))
-				newset.add(c);
-		}
-		
-		NFA newNFA = new NFA();
-		newNFA.addState();
-		newNFA.setStartState(0);
-		int current = 1;
-		for (Character c: newset) {
-			if ( c != EPSILON) {
-				newNFA.addState();
-				newNFA.addTransition(newNFA.getStartState(), current, c);
-				newNFA.addFinalState(current);
-				current++;
-			}
-		}
-		return newNFA;
-	}
 
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
@@ -286,130 +130,4 @@ public class NFA extends FiniteAutomata {
 		NFA nfa1 = new NFA('a');
 		System.out.println(nfa1.getEpsilonClosure(nfa1.getStartState()));
 	}
-	
-//	@SuppressWarnings("unchecked")	
-//	private void testNFA1() {
-//		//Homework 2 problem
-//		numTransitions = 3;
-//		numStates = 13;
-//		startState = 0;
-//		finalStates = new HashSet<Integer>();
-//		finalStates.add(9);
-//		finalStates.add(12);
-//		nfa = new HashSet[numStates][numTransitions];
-//		
-//		for (int i = 0; i < nfa.length; i++) {
-//			for (int j = 0; j < nfa[i].length; j++) {
-//				nfa[i][j] = new HashSet<Integer>();
-//			}
-//		}
-//		
-//		nfa[0][0].add(1);
-//		nfa[0][0].add(6);
-//		
-//		nfa[1][0].add(2);
-//		nfa[1][0].add(3);
-//		
-//		nfa[2][1].add(4);
-//		
-//		nfa[3][2].add(5);
-//		
-//		nfa[4][0].add(1);
-//		nfa[4][0].add(6);
-//		
-//		nfa[5][0].add(1);
-//		nfa[5][0].add(6);
-//		
-//		nfa[6][1].add(7);
-//		
-//		nfa[7][2].add(8);
-//		
-//		nfa[8][0].add(9);
-//		
-//		nfa[9][0].add(10);
-//		
-//		nfa[10][1].add(11);
-//		
-//		nfa[11][2].add(12);
-//		
-//		nfa[12][0].add(10);
-//	}
-//	
-//	@SuppressWarnings("unchecked")	
-//	private void testNFA2() {
-//		//http://www.cs.gsu.edu/~cscskp/Automata/FA/node12.html
-//		numTransitions = 3;
-//		numStates = 3;
-//		startState = 0;
-//		finalStates = new HashSet<Integer>();
-//		finalStates.add(2);
-//		nfa = new HashSet[numStates][numTransitions];
-//		
-//		for (int i = 0; i < nfa.length; i++) {
-//			for (int j = 0; j < nfa[i].length; j++) {
-//				nfa[i][j] = new HashSet<Integer>();
-//			}
-//		}
-//		
-//		nfa[0][1].add(0);
-//		nfa[0][1].add(1);
-//		nfa[0][2].add(0);
-//		
-//		nfa[1][2].add(2);
-//	}
-//
-//	@SuppressWarnings("unchecked")	
-//	private void testNFA3() {
-//		//http://www.jflap.org/tutorial/fa/nfa2dfa/index.html
-//		numTransitions = 3;
-//		numStates = 4;
-//		startState = 0;
-//		finalStates = new HashSet<Integer>();
-//		finalStates.add(3);
-//		nfa = new HashSet[numStates][numTransitions];
-//		
-//		for (int i = 0; i < nfa.length; i++) {
-//			for (int j = 0; j < nfa[i].length; j++) {
-//				nfa[i][j] = new HashSet<Integer>();
-//			}
-//		}
-//		
-//		nfa[0][1].add(1);
-//		nfa[0][1].add(2);
-//		nfa[0][2].add(1);
-//		nfa[0][2].add(3);
-//		
-//		nfa[1][1].add(1);
-//		nfa[1][1].add(3);
-//		
-//		nfa[2][0].add(0);
-//		
-//		nfa[3][1].add(1);
-//		nfa[3][2].add(2);
-//		
-//	}
-//	
-//	@SuppressWarnings("unchecked")	
-//	private void testNFA4() {
-//		//http://www.jflap.org/tutorial/fa/nfa2dfa/index.html
-//		numTransitions = 3;
-//		numStates = 3;
-//		startState = 0;
-//		finalStates = new HashSet<Integer>();
-//		finalStates.add(0);
-//		finalStates.add(2);
-//		nfa = new HashSet[numStates][numTransitions];
-//		
-//		for (int i = 0; i < nfa.length; i++) {
-//			for (int j = 0; j < nfa[i].length; j++) {
-//				nfa[i][j] = new HashSet<Integer>();
-//			}
-//		}
-//		
-//		nfa[0][0].add(1);
-//		nfa[0][0].add(2);
-//		
-//		nfa[1][1].add(0);
-//		nfa[1][2].add(0);
-//	}
 }
