506726aefc5e5f93ed03847d64682002a7312634
Jeremy <jgrebnergt@gmail.com>
2012-11-14 13:56:55 -0500

    Add comments, remove main method.

--- NFA.java
+++ NFA.java
@@ -13,6 +13,11 @@ public class NFA extends FiniteAutomata {
 	private Map<Integer, Set<String>> tokenNames;
 	private static char EPSILON = (char) 169;
 	
+	/**
+	 * Create a new NFA that recognizes a single character.
+	 * 
+	 * @param c The only character this NFA will recognize
+	 */
 	public NFA(char c) {
 		this();
 		
@@ -25,12 +30,22 @@ public class NFA extends FiniteAutomata {
 		addTransition(0, 1, c);
 	}
 	
+	/**
+	 * Initialize a completely empty NFA with no states or transitions.
+	 */
 	public NFA() {
 		nfa = new ArrayList<Map<Character, Set<Integer>>>();
 		transitions = new HashSet<Character>();
 		tokenNames = new HashMap<Integer, Set<String>>();
 	}
 	
+	/**
+	 * Add a transition from one state to another in the NFA on a character.
+	 * 
+	 * @param start The start state
+	 * @param end The end state
+	 * @param transition The character to transition from start to end
+	 */
 	public void addTransition(int start, int end, char transition) {
 		transitions.add(transition);
 		if (!nfa.get(start).containsKey(transition))
@@ -38,18 +53,37 @@ public class NFA extends FiniteAutomata {
 		nfa.get(start).get(transition).add(end);
 	}
 	
+	/**
+	 * Add another empty state to the NFA.
+	 */
 	public void addState() {
 		nfa.add(new HashMap<Character, Set<Integer>>());
 	}
 	
+	/**
+	 * Add an epsilon transition from one state to another.
+	 * 
+	 * @param start The start state
+	 * @param end The end state
+	 */
 	public void addEpsilonTransition(int start, int end) {
 		addTransition(start, end, EPSILON);
 	}
 	
+	/**
+	 * Get all of the states and transitions of the NFA.
+	 * 
+	 * @return The backing data of the NFA.
+	 */
 	public List<Map<Character, Set<Integer>>> getNFA() {
 		return nfa;
 	}
 	
+	/**
+	 * Get all of the possible character transitions in this NFA.
+	 * 
+	 * @return A set of all the possible character transitions in this NFA.
+	 */
 	public Set<Character> getTransitions() {
 		return transitions;
 	}
@@ -62,6 +96,11 @@ public class NFA extends FiniteAutomata {
 		return nfa.size();
 	}
 	
+	/**
+	 * Associate a token name with all of the final states in this NFA.
+	 * 
+	 * @param name The name of the token class
+	 */
 	public void addTokenName(String name) {
 		for (Integer i : finalStates) {
 			if (!tokenNames.containsKey(i)) {
@@ -71,6 +110,11 @@ public class NFA extends FiniteAutomata {
 		}
 	}
 	
+	/**
+	 * Associate a specific state with a token name
+	 * @param state The state to associate the token name with
+	 * @param name The token name
+	 */
 	public void addTokenName(int state, String name) {
 		if (!tokenNames.containsKey(state)) {
 			tokenNames.put(state, new HashSet<String>());
@@ -78,6 +122,11 @@ public class NFA extends FiniteAutomata {
 		tokenNames.get(state).add(name);
 	}
 	
+	/**
+	 * Get a set of all token names associated with a final state
+	 * @param state The state to get the token names of
+	 * @return A set of token names
+	 */
 	public Set<String> getTokenNames(int state) {
 		if (!finalStates.contains(state)) {
 			return null;
@@ -86,10 +135,25 @@ public class NFA extends FiniteAutomata {
 		}
 	}
 	
+	/**
+	 * Get all of the states you can get to from a start state and a character.
+	 * 
+	 * @param state The state to start from.
+	 * @param transition The character to transition with. 
+	 * @return A set of states transitioned to.
+	 */
 	public Set<Integer> getNextStates(int state, char transition) {
 		return nfa.get(state).get(transition);
 	}
 	
+	/**
+	 * Get all of the states you can get to from a set of start states
+	 * and a character.
+	 * 
+	 * @param states The set of states to start from.
+	 * @param transition The character to transition with. 
+	 * @return  A set of states transitioned to.
+	 */
 	public Set<Integer> getNextStates(Set<Integer> states, char transition) {
 		Set<Integer> nextStates = new HashSet<Integer>();
 		for (Integer state : states) {
@@ -100,6 +164,10 @@ public class NFA extends FiniteAutomata {
 		return nextStates;
 	}
 	
+	/**
+	 * @param states A set of start states
+	 * @return A set of states resulting from the epsilon closure.
+	 */
 	public Set<Integer> getEpsilonClosure(Set<Integer> states) {
 		Set<Integer> closure = new HashSet<Integer>();
 		for (int i : states) {
@@ -108,6 +176,11 @@ public class NFA extends FiniteAutomata {
 		return closure;
 	}
 	
+	/**
+	 * 
+	 * @param state The state to start from.
+	 * @return A set of states resulting from the epsilon closure.
+	 */
 	public Set<Integer> getEpsilonClosure(int state) {
 		Set<Integer> visited = new HashSet<Integer>();
 		Set<Integer> closure = new HashSet<Integer>();
@@ -132,7 +205,6 @@ public class NFA extends FiniteAutomata {
 		return closure;
 	}
 
-	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		sb.append(String.format("Start state: %d\n", startState));
@@ -151,10 +223,5 @@ public class NFA extends FiniteAutomata {
 			i++;
 		}
 		return sb.toString();
-	}
-	
-	public static void main(String[] args) {
-		NFA nfa1 = new NFA('a');
-		System.out.println(nfa1.getEpsilonClosure(nfa1.getStartState()));
 	}
 }
