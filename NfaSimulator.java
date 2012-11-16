5d28982ecb3c22c63781a415da3993621f2633d9
akbar <akbardhannani@gmail.com>
2012-11-15 22:21:56 -0500

    Updating nfa simulator, not functional yet

--- NfaSimulator.java
+++ NfaSimulator.java
@@ -1,26 +1,35 @@
+import java.util.List;
+import java.util.LinkedList;
 import java.util.Set;
 
 
 public class NfaSimulator {
 	public static void main (String[] args) throws Exception{
 		SpecificationReader sr = new SpecificationReader("specification.txt");
-		String test = "1234abc";
-		NFA nfa = sr.run();
-		int startState = nfa.getStartState();
-		Set<Integer> states = nfa.getEpsilonClosure(startState);
+		String test = "1234ab";
 		int counter = 0;
+		int transitions = 0;
+		int clonesMade = 0;
+		LinkedList<Integer> maxClones = new LinkedList<Integer>();
+		NFA nfa = sr.run();
+		List<Integer> statesList = new LinkedList<Integer>();
+		int startState = nfa.getStartState();
+		Set<Integer> states = nfa.getNextStates(startState, (char)169);
+		statesList.addAll(states);
+		states = nfa.getNextStates(startState, test.charAt(counter));
+		statesList.addAll(states);
+		transitions = transitions + states.size();
+		clonesMade = clonesMade + states.size();
+		String trail = "";
+		String ident = "";
+		
 		while(counter<test.length()){
-			Set<Integer> nextStates = nfa.getNextStates(states, test.charAt(counter));
-			nextStates = nfa.getEpsilonClosure(nextStates);
-			System.out.println(nextStates);
-			for(Integer state : nextStates){
-				if (nfa.getTokenNames(state)!=null){
-					System.out.println(nfa.getTokenNames(state));
-				}
+			counter++;	
+			List<Integer> trans = new LinkedList<Integer>();
+			for(Integer state: statesList){
+				Set<Integer>nextStates = nfa.getNextStates(state, (char)169);
+				
 			}
-			states = nfa.getEpsilonClosure(nextStates);
-			counter++;
-			
 		}
 	}
 }
