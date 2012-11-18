cb22e28054ce8180c62c4d3da26a28a7286475d8
akbar <akbardhannani@gmail.com>
2012-11-17 22:59:13 -0500

    Commented the nfa simulator

--- NFASimulator.java
+++ NFASimulator.java
@@ -27,14 +27,14 @@ public class NFASimulator {
 		Set<Integer> cache = new HashSet<Integer>();
 		String currentIdent = "";
 		int totalTransitions = nextStates.size();
-		while(peek() != -1 && peek() != 65535){
+		while(peek() != -1 && peek() != 65535){ //while there are more characters we keep getting them and testing them
 			curChar = (char)reader.read();
 			cache.addAll(nextStates);
 			maxClones.add(nextStates.size());
 			Set<Integer> currentTransitions = nfa.getNextStates(nextStates, curChar);
 			nextStates.clear();
 			nextStates.addAll(currentTransitions);
-
+			//Take all the epsilon moves we can from the states we just added using curChar
 			nextStates.addAll(nfa.getEpsilonClosure(currentTransitions));
 
 			totalTransitions += nextStates.size();
@@ -44,8 +44,9 @@ public class NFASimulator {
 					allNull = false;
 				}
 			}
+			//If our current character yeilded to no valid states, or to null states, we should take action as it is invalid
 			if(nextStates.size()==0|| allNull){
-				if(validToken.length()>0 && !currentIdent.equals("INVALID")){ //If there is a token that is valid
+				if(validToken.length()>0 && !currentIdent.equals("INVALID")){ //If there is a token that is valid in the current list
 					if(peek()==-1 || peek()==65535){
 						System.out.println(currentIdent + validToken);
 						currentIdent = "INVALID";
@@ -57,6 +58,8 @@ public class NFASimulator {
 					currentTransitions.clear();
 					currentTransitions.addAll(nfa.getNextStates(nextStates, next));
 					currentTransitions.addAll(nfa.getEpsilonClosure(currentTransitions));
+					//Here we see if the next character makes it valid, if so then we leave it be and push it back, if not then we add on
+					// to the current list of invalid characters and move on
 					if(currentTransitions.size()==0){
 						validToken.append(curChar);
 						validToken = new StringBuilder();
@@ -67,11 +70,12 @@ public class NFASimulator {
 						nextStates.addAll(nfa.getEpsilonClosure(curState));
 						totalTransitions += nextStates.size();
 					}
-					else{
+					else{//Current token is fine, add it and unread the token we just read.
 						validToken.append(curChar);
 						reader.unread(next);	
 					}
-				}
+				}//if we encountered an illegal character while nothing before this is illegal we mark our current
+				// copy of token as invalid
 				else if(currentIdent.equals("")){
 					if(validToken.length()!=0)
 					System.out.println("Valid char should be empty" + validToken);
@@ -90,7 +94,8 @@ public class NFASimulator {
 						if(nfa.getTokenNames(st)!=null){
 							nulls = false;
 						}
-					}
+					}//If the current token is illegal but the character we just read is legal
+					//we should end the current token and start off with the new token(so we push it back)
 					if(currentTransitions.size()!=0 || !nulls){
 						validToken.append(curChar);
 						nextStates.clear();
@@ -103,7 +108,9 @@ public class NFASimulator {
 					currentTransitions.clear();
 					currentTransitions.addAll(nfa.getNextStates(begin, curChar));
 					currentTransitions.addAll(nfa.getEpsilonClosure(currentTransitions));
-					
+					//We just check to make sure that if we can go somewher with the new character
+					// we restart our process from the beginning as the next character\
+					//is a different type, we push it back so we read it next iteration
 					if(currentTransitions.size()!=0){
 						nextStates.clear();
 						nextStates.add(curState);
@@ -140,19 +147,19 @@ public class NFASimulator {
 						validToken.append(curChar);
 					}
 				}
-			}
+			}//If the current character leads us to a valid state
 			else{
-				validToken.append(curChar);
+				validToken.append(curChar); //Add teh character to our token type
 				for(int tran: nextStates){
 					if(nfa.getTokenNames(tran)!= null && nfa.getTokenNames(tran).size()!=0){
-						currentIdent = nfa.getTokenNames(tran).toString();
+						currentIdent = nfa.getTokenNames(tran).toString(); //update our current token type to reflect changes
 						currentIdent = currentIdent.replace("[", "");
 						currentIdent = currentIdent.replace("]", "");
 						break;
 					}
 				}
 				
-				if(peek()==-1 || peek()==65535){
+				if(peek()==-1 || peek()==65535){ //If the next character makes us invalid we should end the current valid and start over
 					if(!isWhitespace(validToken.toString()))
 					System.out.println(currentIdent + " "+ validToken.toString().trim());
 					currentIdent = "";
@@ -188,6 +195,7 @@ public class NFASimulator {
 		reader.unread(c);
 		return c;
 	}
+	//Helps to figure out if a string is whitespace or not
 	public boolean isWhitespace(String s) {
 		for (int i = 0; i < s.length(); i++) {
 			char c = s.charAt(i);
