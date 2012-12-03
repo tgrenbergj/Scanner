179fb631ddcaf5d1a3aabb046910370d9057b21d
Jeremy <jgrebnergt@gmail.com>
2012-12-02 22:11:47 -0500

    LL1Parser did not continue popping non-terminals

--- LL1Parser.java
+++ LL1Parser.java
@@ -13,38 +13,43 @@ public class LL1Parser {
 	}
 	
 	public boolean parseToken(String token){
-		boolean parsed = false;
 		String top = stack.peek();
+		
 		if(top.equals(token)&& token.equals("$")){
 			stack.pop();
 			return true;
 		}
-		if(grammar.isTerminal(top)){
-			if(token.equals(top)){
-				stack.pop();
-				return true;
-			}
-			else{
-				System.out.println("Error");
+		
+		while (grammar.isNonTerminal(top)){
+			System.out.println(stack);
+			String[] rule =  grammar.table[grammar.nontermMap.get(top)][grammar.termMap.get(token)];
+			if (rule == null) {
+				System.err.println("terminal");
 				return false;
 			}
-		}
-		 if (grammar.isNonTerminal(top)){
-			String[] a=  grammar.table[grammar.nontermMap.get(top)][grammar.termMap.get(token)];
 			stack.pop();
-			pushOnStack(a);
+			if (!rule[0].equals("EPSILON"))
+				pushOnStack(rule);
+			top = stack.peek();
+		}
+		
+		if(token.equals(top)){
+			System.out.println(stack);
+			stack.pop();
 			return true;
 		}
 		else{
-			System.out.println("Error on top of stack");
+			System.out.println("Error");
+			return false;
 		}
-		return parsed;
 	}
+	
 	public void pushOnStack(String[] stuff){
 		for(int i = stuff.length-1; i>=0;i--){
 			stack.push(stuff[i]);
 		}
 	}
+	
 	/**
 	 * Temporary main method
 	 */
@@ -52,9 +57,9 @@ public class LL1Parser {
 		Grammar grammar = GrammarReader.read("src\\input_phase2\\microsoft_grammar.txt");
 		LL1Parser parser = new LL1Parser(grammar);
 		
-		String[] tokens = {"int", "+", "int", "*", "int"};
+		String[] tokens = {"int", "+", "int", "*", "int", "$"};
 		for (int i = 0; i < tokens.length; i++) {
-			System.out.println(parser.parseToken(tokens[i]));
+			parser.parseToken(tokens[i]);
 		}
 
 	}
