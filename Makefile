84f9d6b9a8bc7f58588459e40d3b9a17ab408863
Jeremy <jgrebnergt@gmail.com>
2012-12-07 22:04:35 -0500

    Updated Makefile and runner for phase2

--- Makefile
+++ Makefile
@@ -13,3 +13,12 @@ clean:
 	
 tar: DFA.java DFAWalker.java FiniteAutomata.java NFA.java NFAConverter.java NFARunner.java NFASimulator.java NFATools.java RecursiveDescentParser.java Runner.java SpecificationReader.java
 	tar cvzf project.phase1.jgrebner3.tar.gz *.java Makefile README *.sh input output
+
+phase2_move:
+	cp *.java submission-phase2/src
+
+phase2_clean:
+	rm -rf project.phase2.jgrebner3.tar.gz submission-phase2/src/*.java submission-phase2/src/*.class
+
+phase2_package: phase2_clean phase2_move
+	tar cvzf project.phase2.jgrebner3.tar.gz submission-phase2
--- MiniRERunner.java
+++ MiniRERunner.java
@@ -1,17 +1,12 @@
-
-
 public class MiniRERunner {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		//Commented for testing
-//		if(args.length<1 || args.length>0){
-//			System.out.println("Incorrect number of parameters");
-//			return;
-//		}
-		MiniREParser mrp = new MiniREParser("src\\input_phase2\\script.txt", "src\\input_phase2\\minire_spec.txt");
+
+		if(args.length<1 || args.length>1){
+			System.out.println("Usage: java MiniRERunner script.txt");
+			return;
+		}
+		MiniREParser mrp = new MiniREParser(args[0], "minire_spec.txt");
 		mrp.run();
 	}
 
