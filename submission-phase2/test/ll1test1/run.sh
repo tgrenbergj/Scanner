e406653c12d1e8bda0872265931217685e0882ab
Jeremy <jgrebnergt@gmail.com>
2012-12-08 09:51:15 -0500

    Add LL(1) test

---
+++ submission-phase2/test/ll1test1/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp ../../src LL1ParserDriver grammar.txt grammar_info.txt minire_spec.txt script.txt ASCII-STR REGEX ID $
---
+++ submission-phase2/test/ll1test2/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -cp ../../src LL1ParserDriver grammar.txt grammar_info.txt spec.txt script.txt
